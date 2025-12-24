c514624ee126e05a3be30538174e40df8b15a4d8
Algets07 <algetes2004@gmail.com>
2025-12-24 10:44:47 +0530

    custompanel

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 from .models import movie
 # Register your models here.
 
-admin.site.register(movie)
+from django.utils.html import format_html
+
+class moviecustomPannel(admin.ModelAdmin):
+    list_display = ('name','desc','img','id')
+
+    def img(self,obj):
+        return format_html('<img src="{0}" width="200px" height="100px">'.format(obj.img.url))
+
+
+
+
+
+
+admin.site.register(movie,moviecustomPannel)
